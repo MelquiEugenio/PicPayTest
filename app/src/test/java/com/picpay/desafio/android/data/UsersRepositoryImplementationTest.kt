acb28c1d6b151ba7a3670fd4ab8165be6f23d556
Melquisedeque Silva <melquiapps@gmail.com>
2022-03-15 15:19:47 -0300

    REFACTOR: Arch for tests.

---
+++ app/src/test/java/com/picpay/desafio/android/data/UsersRepositoryImplementationTest.kt
@@ -0,0 +1,20 @@
+package com.picpay.desafio.android.data
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class UsersRepositoryImplementationTest {
+
+    @Test
+    fun getUsersFromApi() {
+    }
+
+    @Test
+    fun saveUsers() {
+    }
+
+    @Test
+    fun getSavedUsers() {
+    }
+}
